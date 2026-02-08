e351b67183ad74a7558d78b297f1ad2c231504fd
DeekshaGangwar7251 <gangwardeeksha2301@gmail.com>
2026-02-08 19:38:51 +0530

    Add signup controller

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,6 +2,7 @@ const User=require("../models/User");
 const OTP=require("../models/OTP");
 
 //send otp
+
 exports.sendOTP=async(req,res)=>{
 
   try{
@@ -57,4 +58,107 @@ exports.sendOTP=async(req,res)=>{
         message:error.message,
     })
   }
+};
+
+//signup
+
+//data fetch from request ki body
+exports.signUp=async(req,res)=>{
+
+try{  
+const{
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword,
+    accountType,
+    contactNumber,
+    otp   
+}=req.body;
+
+//validate
+if(!firstName ||!lastName||!email||!password||!confirmPassword||!otp){
+    return res.status(403).json({
+        success:false,
+        message:'All fields are required',
+    })
+}
+
+//password match 
+if(password!==confirmPassword){
+    return res.status(400).json({
+        success:false,
+        message:'password and confirmPassword value does not match please try again',
+    });
+}
+
+//check user already exist or not
+
+const existingUser=await User.findOne({email});
+if(existingUser){
+    return res.status(400).json({
+        success:false,
+        message:'User is already registered',
+    });
+}
+
+//find most recent otp stored for the user
+
+const recentOtp=await OTP.find({email}).sort({createdAt:-1}).limit(1);
+console.log(recentOtp);
+//validate OTP
+if(recentOtp.length==0){
+    //otp not found
+    return res.status(400).json({
+        success:false,
+        message:'OTP not found',
+    })
+}
+else if(otp!==recentOtp.otp){
+   //Invalid OTP
+   return res.status(400).json({
+    success:false,
+    message:'Invalid OTP',
+   });
+}
+
+//hash password
+const hashedPassword=await bcrypt.hash(password,10);
+
+//entry create in db
+
+const profileDetails=await Profile.create({
+    gender:null,
+    dateOfBirth:null,
+    about:null,
+    contactNumber:null,
+});
+
+const user=await User.create({
+    firstName,
+    lastName,
+    email,contactNumber,
+    password:hashedPassword,
+    accountType,
+    additionalDetails:profileDetails._id,
+    image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstName}${lastName}`
+})
+
+return res.status(400).json({
+    success:true,
+    message:'User is registered successfully',
+    user,
+})
+
+} catch(error){
+    console.log(error);
+    return res.status(500).json({
+        success:false,
+        message:'User cannot registered ,please try again',
+    });
+ }
+
+
+
 }
