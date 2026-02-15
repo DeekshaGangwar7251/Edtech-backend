d08c04fdd10038c33e69ed6e70cb7e15596a9b90
DeekshaGangwar7251 <gangwardeeksha2301@gmail.com>
2026-02-15 12:47:16 +0530

    add category page details

--- controllers/Category.js
+++ controllers/Category.js
@@ -48,3 +48,45 @@ exports.showAllcategories=async(req,res)=>{
     }
 };
 
+//category page details
+
+ exports.categoryPageDetails=async(req,res)=>{
+  try{
+       //get category id
+       const {categoryId}=req.body;
+      // get courses for specified id
+      const selectedCategory=await Category.findById(categoryId)
+                                           .populate("courses")
+                                           .exec();
+      //validate
+      if(!selectedCaegory){
+         return res.status(404).json({
+        success:false,
+        message:'Data not found',
+      });
+     }
+
+      //get courses for diff category
+      const differentCategories=await Category.find({
+                                _id:{$ne:categoryId},
+                                 })
+                                 .populate("courses")
+                                 .exec();
+      //get top selling courses
+
+      //return 
+       return res.status(500).json({
+        success:true,
+        data:{
+          selectedCategory,
+          differentCategeories,
+        },     
+    });
+  }
+  catch(error){
+     return res.status(500).json({
+        success:false,
+        message:error.message,
+    });
+  }
+ }
